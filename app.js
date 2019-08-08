530a5d68ac987d8928da6b3620faa1304fe7ba6f
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-08 16:30:14 -0300

    fixing errors in implemented code

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const department_controller = require('./department_controller');
 const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/http_app', { useNewUrlParser: true });
 
