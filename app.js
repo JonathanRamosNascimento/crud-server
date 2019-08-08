834ef612470030935659349d0bfb5a1439867324
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-08-08 20:38:00 -0300

    Creating CRUD methods for the product

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const department_controller = require('./department_controller');
+const product_controller = require('./product_controller');
 const app = express();
 
 app.use(bodyparser.json());
@@ -12,6 +13,6 @@ app.use(cors());
 mongoose.connect('mongodb://localhost:27017/http_app', { useNewUrlParser: true });
 
 app.use('/departments', department_controller);
-// app.use('/products', product_controller);
+app.use('/products', product_controller);
 
 app.listen(3000);
